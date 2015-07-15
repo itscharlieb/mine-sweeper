9e94dff7a7271c9c3de8b2f930275e7cd57070a8
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2015-07-14 23:29:27 -0400

    setup initial code structure

---
+++ src/model/Board.java
@@ -0,0 +1,20 @@
+/**
+ * @author Charlie Bloomfield
+ * Jul 14, 2015
+ */
+
+package model;
+
+/**
+ * 
+ */
+public class Board {
+
+	/**
+	 * 
+	 */
+	public Board() {
+		//TODO
+	}
+
+}
---
+++ src/model/Cell.java
@@ -0,0 +1,20 @@
+/**
+ * @author Charlie Bloomfield
+ * Jul 14, 2015
+ */
+
+package model;
+
+/**
+ * 
+ */
+public class Cell {
+
+	/**
+	 * 
+	 */
+	public Cell() {
+		//TODO
+	}
+
+}
