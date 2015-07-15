bcb7a9e12bda1aa5dc2c61d12d34147b741317ff
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2015-07-14 23:56:19 -0400

    added class stubs in gui and ai packages

---
+++ src/ai/AIAgent.java
@@ -0,0 +1,20 @@
+/**
+ * @author Charlie Bloomfield
+ * Jul 14, 2015
+ */
+
+package ai;
+
+/**
+ * 
+ */
+public class AIAgent {
+
+	/**
+	 * 
+	 */
+	public AIAgent() {
+		//TODO
+	}
+
+}
---
+++ src/gui/GameFrame.java
@@ -0,0 +1,22 @@
+/**
+ * @author Charlie Bloomfield
+ * Jul 14, 2015
+ */
+
+package gui;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+public class GameFrame extends JFrame {
+
+	/**
+	 * 
+	 */
+	public GameFrame() {
+		//TODO
+	}
+
+}
