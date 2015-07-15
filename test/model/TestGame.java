c9fe82050ed8396256cfd710922ddf7c2dc077ed
Charles Bloomfield <charles.bloomfield@mail.mcgill.ca>
2015-07-14 23:54:16 -0400

    added testing stubs

---
+++ test/model/TestGame.java
@@ -0,0 +1,22 @@
+/**
+ * @author Charlie Bloomfield
+ * Jul 14, 2015
+ */
+
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class TestGame {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
